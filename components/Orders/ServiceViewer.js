330d114292b295c6ea02c8661915847fcc57c3ba
avikhandakar-dev <avikhandakar@gmail.com>
2023-05-30 19:54:11 +0600

    bug fix

--- components/Orders/ServiceViewer.js
+++ components/Orders/ServiceViewer.js
@@ -1,9 +1,4 @@
-import { formatSelectedServices } from "@lib/utils";
-
 const ServiceViewer = ({ selectedServices, facilites }) => {
-  const services = selectedServices
-    ? formatSelectedServices(selectedServices)
-    : [];
   return (
     <div className="card w-full bg-base-200 shadow-xl overflow-hidden h-full border border-warning rounded">
       <div className="px-4 py-3 bg-warning text-warning-content card-title">
@@ -12,14 +7,7 @@ const ServiceViewer = ({ selectedServices, facilites }) => {
       <div className="card-body max-w-xl mx-auto">
         <div>
           <div className="flex gap-4 flex-wrap">
-            {services?.map((service, index) => (
-              <div
-                key={service.id}
-                className="badge badge-warning badge-outline badge-lg"
-              >
-                {index + 1}. {service}
-              </div>
-            ))}
+            {selectedServices.join(", ")}
           </div>
         </div>
         <div className="mt-4">
